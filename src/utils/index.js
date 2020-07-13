690eefe66be365596324f5bef24f8f78596ea836
assmdx <assmdx183@163.com>
2020-07-14 01:12:07 +0800

    refactor: 💡 util.js迁移至src/utils/目录下

---
+++ src/utils/index.js
@@ -0,0 +1,35 @@
+const store = window.localStorage || {};
+
+function getConfig(key) {
+  let wordkit = store.getItem('wordkit') ? JSON.parse(store.getItem('wordkit')) : {};
+  if (key) {
+    return wordkit[key]
+  } else {
+    return wordkit
+  }
+}
+
+function setConfig(key, value) {
+  let wordkit = store.getItem('wordkit') ? JSON.parse(store.getItem('wordkit')) : {};
+  if (key) {
+    wordkit[key] = value;
+  } else {
+    wordkit = value
+  }
+
+
+  store.setItem('wordkit', JSON.stringify(wordkit))
+}
+
+
+//子进程之间通信的消息体的编码和解码函数
+const {
+  ipcRenderer
+} = require('electron')
+
+function sendMsgBetweenRender(EVENT_TYPE, MSG) {
+  ipcRenderer.send('msgBetweenRender', {
+    EVENT_TYPE,
+    MSG
+  })
+}
