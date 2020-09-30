6fc4bf2dfd9f83076cb92c5f6352587d0e1cbb47
renzhenchuan <renzhenchuan@bytedance.com>
2020-09-30 08:35:19 +0800

    fix: 🐛 mac下图标丢失

--- src/main/ployfill.mac.ts
+++ src/main/ployfill.mac.ts
@@ -2,5 +2,5 @@ import { app } from 'electron';
 import path from 'path';
 
 export const ployfill = () => {
-    app.dock.setIcon(path.join(__dirname, '../icon.mac.dock.png'));
+    app.dock.setIcon(path.join(__dirname, '../icon/icon.mac.dock.png'));
 };
